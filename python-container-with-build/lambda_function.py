d9f869613610f5739030941ffb6c100ed15d5a24
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2024-05-20 21:18:31 +0530

    add

--- python-container-with-build/lambda_function.py
+++ python-container-with-build/lambda_function.py
@@ -5,7 +5,7 @@ import sys
 def handler(event, context):
 
     body = {
-        "message": f"Go Serverless v3.0! Your function executed successfully! and runtime is {sys.version}"
+        "message": f"1 Go Serverless v3.0! Your function executed successfully! and runtime is {sys.version}"
     }
 
     return {"statusCode": 200, "body": json.dumps(body)}
---
+++ python-rest-api-sqs/handler.py
@@ -0,0 +1,28 @@
+import json
+# import requests
+
+def hello(event, context):
+    # body = {
+    #     "message": "Go Serverless v3.0! Your function executed successfully!",
+    #     "input": event,
+    # }
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!"
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
+
+# def get_dummy_data(event, context):
+#     dummy_data = requests.get('https://catfact.ninja/fact')
+
+#     response = {
+#         "statusCode": dummy_data.status_code,
+#         "body": dummy_data.text
+#     }
+
+#     return response
+
+    # response = {
+    #     "statusCode": res.status_code,
+    #     "body": res.text
+    # }
